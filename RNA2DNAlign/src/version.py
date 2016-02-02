ed645818eb3672a2756ff996685c4c19d30d98bc
Nathan Edwards <edwardsnj@gmail.com>
2016-02-02 10:10:59 -0500

    Rename RNA2DNA.

--- RNA2DNAlign/src/version.py
+++ RNA2DNAlign/src/version.py
@@ -1,6 +1,6 @@
 #!/bin/env python27
 VERSION = '1.0.9'
-PROGRAMS = 'readCounts.py LoH.py RNA2DNAlign.py exonicFilter.py snv_computation.py'
+PROGRAMS = 'readCounts.py LoH.py RNA2DNA.py exonicFilter.py snv_computation.py'
 if __name__ == '__main__':
     import sys
     print eval(sys.argv[1])
