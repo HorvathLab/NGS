00a562de40d35eff14e72a6f13f36ef2e4d9373f
Nathan Edwards <edwardsnj@gmail.com>
2020-10-07 21:00:11 -0400

    tweak versioning

--- ReadCounts/src/version.py
+++
@@ -1,7 +0,0 @@
-#!/bin/env python
-VERSION = '2.2.0'
-PROGRAMS = 'readCounts.py phasedReadCounts.py readCountsMatrix.py scReadCounts.py'
-INCLUDES = 'common'
-if __name__ == '__main__':
-    import sys
-    print(eval(sys.argv[1]))
--- SCReadCounts/src/version.py
+++
@@ -1,7 +0,0 @@
-#!/bin/env python
-VERSION = '1.0.0'
-PROGRAMS = 'readCountsMatrix.py scReadCounts.py'
-INCLUDES = 'common ReadCounts'
-if __name__ == '__main__':
-    import sys
-    print(eval(sys.argv[1]))
