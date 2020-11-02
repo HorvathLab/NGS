0e9a5ffdc202a524626a1133f787c06772fad9db
Nathan Edwards <edwardsnj@users.noreply.github.com>
2020-11-02 15:24:09 -0500

    Update singlecell.sh

--- SCReadCounts/data/singlecell.sh
+++ SCReadCounts/data/singlecell.sh
@@ -8,15 +8,14 @@ rm -f singlecell-output.tsv singlecell-output.cnt.matrix.tsv singlecell-output.v
 #
 # readCounts -s "singlecell_222_5_chr17.txt" -r "singlecell_chr17.bam" -m 0 -G "UMITools" -o "singlecell-output.tsv"
 # readCountsMatrix -c "singlecell-output.tsv" -M Ref:Var -o "singlecell-output.cnt.matrix.tsv"                   
-# readCountsMatrix -c "singlecell-output.tsv" -M VAF -m 3 -o "singlecell-output.vaf.matrix.tsv"
+# readCountsMatrix -c "singlecell-output.tsv" -M VAF -m 3 -o "singlecell-output.vaf-m3.matrix.tsv"
 #
-# readCountsMatrix -c "singlecell-output.tsv" -M VAF -m 5 -o "singlecell-output.vaf.matrix.tsv"
+# readCountsMatrix -c "singlecell-output.tsv" -M VAF -m 5 -o "singlecell-output.vaf-m5.matrix.tsv"
 #
 
 set -x
-../bin/scReadCounts -r singlecell_chr17.bam -s singlecell_222_5_chr17.txt -m 3 -o singlecell-output_min3.tsv
+../bin/scReadCounts -r singlecell_chr17.bam -s singlecell_222_5_chr17.txt -m 3 -o singlecell-output.tsv
 
-# Regenerate only the VAF matrix
-rm -f singlecell-output.vaf.matrix.tsv
-../bin/scReadCounts -r singlecell_chr17.bam -s singlecell_222_5_chr17.txt -m 5 -o singlecell-output_min5.tsv
+# Regenerate the VAF matrix (different output name: singlecell-output.vaf-m5.matrix.tsv)
+../bin/scReadCounts -r singlecell_chr17.bam -s singlecell_222_5_chr17.txt -m 5 -o singlecell-output.tsv
 
