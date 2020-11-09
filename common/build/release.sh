def703595de261852fa05d45365355f4de15fa90
Nathan Edwards <edwardsnj@gmail.com>
2020-11-09 12:59:25 -0500

    update release script

--- common/build/release.sh
+++ common/build/release.sh
@@ -3,5 +3,5 @@ if [ "$1" = "" ]; then
    echo "Usage: release.sh <release-tag>"
    exit 1
 fi
-echo md5sum $1.*tgz > $1.md5
-echo gh release create $1 -F $1.txt $1.*tgz $1.md5
+md5sum $1.*tgz > $1.md5
+gh release create $1 -F $1.txt $1.*tgz $1.md5
