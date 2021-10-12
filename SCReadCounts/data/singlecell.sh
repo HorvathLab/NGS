43f4654be6c22e0148fc033f64eb7a923bd8b142
Nathan Edwards <edwardsnj@gmail.com>
2021-10-12 11:41:39 -0400

    STARsolo cell barcodes / read group example for SCReadCounts

--- SCReadCounts/data/singlecell.sh
+++ SCReadCounts/data/singlecell.sh
@@ -1,21 +1,39 @@
 #!/bin/sh
 
 # Clean up previous runs...
-rm -f singlecell-output.tsv singlecell-output.cnt.matrix.tsv singlecell-output.vaf-m*.matrix.tsv
+rm -f singlecell-output.{tsv,*.matrix.tsv}
+rm -f singlecell2-output.{tsv,*.matrix.tsv}
+rm -f singlecell2-all-output.{tsv,*.matrix.tsv}
+
+set -x
 
 #
 # Equivalent to running these three commands...
 #
-# readCounts -s "singlecell_222_5_chr17.txt" -r "singlecell_chr17.bam" -m 0 -G "UMITools" -o "singlecell-output.tsv"
+# UMI-tools is the default cell-barcode strategy
+#
+# readCounts -s "singlecell_222_5_chr17.txt" -r "singlecell_chr17.bam" -m 0 -G "UMI-tools" -o "singlecell-output.tsv"
 # readCountsMatrix -c "singlecell-output.tsv" -M Ref:Var -o "singlecell-output.cnt.matrix.tsv"                   
 # readCountsMatrix -c "singlecell-output.tsv" -M VAF -m 3 -o "singlecell-output.vaf-m3.matrix.tsv"
 #
 # readCountsMatrix -c "singlecell-output.tsv" -M VAF -m 5 -o "singlecell-output.vaf-m5.matrix.tsv"
 #
-
-set -x
 ../bin/scReadCounts -r singlecell_chr17.bam -s singlecell_222_5_chr17.txt -m 3 -o singlecell-output.tsv
 
+#
 # Regenerate the VAF matrix (different output name: singlecell-output.vaf-m5.matrix.tsv)
+#
 ../bin/scReadCounts -r singlecell_chr17.bam -s singlecell_222_5_chr17.txt -m 5 -o singlecell-output.tsv
+
+#
+# STARsolo example
+#
+../bin/scReadCounts -r singlecell2_117.bam -s singlecell2_117_snvs.txt -m 5 \
+                    -G STARsolo -b singlecell2_117_barcodes.tsv -o singlecell2-output.tsv
+
+#
+# Override accept list for barcodes, accept all barcodes
+#
+../bin/scReadCounts -r singlecell2_117.bam -s singlecell2_117_snvs.txt -m 5 \
+                    -G STARsolo -b None -o singlecell2-all-output.tsv
 
