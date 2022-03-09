dae9081b7de22f822e38c85137810c2cc1a804be
Nathan Edwards <edwardsnj@gmail.com>
2022-03-09 15:43:45 -0500

    ensure aligned reads are output in the same order as the input bamfile

--- common/src/intervalscan.py
+++ common/src/intervalscan.py
@@ -19,7 +19,7 @@ class IntervalScan(object):
             if al.reference_name != current_name:
                 while len(current_ends) > 0:
                     pos,ind,al1 = heappop(current_ends)
-                    self.process_end(al1)
+                    self.process_end(ind,al1)
                 # reset
                 current_name = al.reference_name
                 current_ends = []
@@ -29,23 +29,23 @@ class IntervalScan(object):
             while (len(current_ends) > 0 and current_ends[0][0] < current_start):
                 pos,ind,al1 = heappop(current_ends)
                 assert pos < current_start
-                self.process_end(al1)
+                self.process_end(ind,al1)
             heappush(current_ends,(al.reference_end,alindex,al))
+            self.process_start(alindex,al)
             alindex += 1
-            self.process_start(al)
             last_start = current_start
         while len(current_ends) > 0:
             pos,ind,al1 = heappop(current_ends)
-            self.process_end(al1)
+            self.process_end(ind,al1)
         self.process_done()
 
     def process_init(self):
         pass
  
-    def process_start(self,alignment):
+    def process_start(self,index,alignment):
         pass
 
-    def process_end(self,alignment):
+    def process_end(self,index,alignment):
         pass
 
     def process_done(self):
@@ -60,9 +60,9 @@ class Intervals(IntervalScan):
         self.depth = 0
         self.current = set()
 
-    def process_start(self,al):
+    def process_start(self,alindex,al):
         self.depth += 1
-        self.current.add(al)
+        self.current.add((alindex,al))
         if self.depth < self.mindepth:
             return
         if self.depth == self.mindepth:
@@ -71,9 +71,9 @@ class Intervals(IntervalScan):
             self.maxdepth = self.depth
         else:
             self.maxdepth = max(self.maxdepth,self.depth)
-            self.interval_current.add(al)
+            self.interval_current.add((alindex,al))
 
-    def process_end(self,al):
+    def process_end(self,alindex,al):
         if self.depth == self.mindepth:
             interval_end = al.reference_end
             print(al.reference_name,
@@ -83,7 +83,7 @@ class Intervals(IntervalScan):
                   interval_end-self.interval_start,
                   self.maxdepth)
         self.depth -= 1
-        self.current.remove(al)
+        self.current.remove((alindex,al))
 
 class MinDepthBAM(Intervals):
 
@@ -91,15 +91,15 @@ class MinDepthBAM(Intervals):
         super(MinDepthBAM,self).process_init()
         self.outreads = set()
 
-    def process_end(self,al):
+    def process_end(self,alindex,al):
         if self.depth == self.mindepth:
             self.outreads.update(self.interval_current)
-        super(MinDepthBAM,self).process_end(al)
+        super(MinDepthBAM,self).process_end(alindex,al)
 
     def process_done(self):
         outfilename = os.path.split(self.filename.rsplit('.',1)[0] + ".d%d.bam"%(self.mindepth,))[1]
         outbam = pysam.AlignmentFile(outfilename,'wb',template=self.samfile)
-        for al1 in sorted(self.outreads,key=lambda al1: (al1.reference_id,al1.reference_start,al1.reference_end)):
+        for alindex1,al1 in sorted(self.outreads):
             outbam.write(al1)
         outbam.close()
         pysam.index(outfilename)
