57279ec770b7416be3af6f5cf5521335a2e8913a
Nathan Edwards <edwardsnj@gmail.com>
2022-06-15 17:05:31 -0400

    multiple-region support for BAM file split

--- common/src/split.py
+++ common/src/split.py
@@ -1,9 +1,10 @@
 
 from pysamimport import pysam
 import re, os, hashlib
+from collections import defaultdict
 
 class SplitBAM(object):
-    def __init__(self,bamfile,readgroups,batchsize=10,directory='.',index=False,region=None,limit=None):
+    def __init__(self,bamfile,readgroups,batchsize=10,directory='.',index=False,regions=None,limit=None):
         self.bamfile = os.path.realpath(bamfile)
         self.bambase,self.bamextn = self.bamfile.rsplit('.',1)
         self.bambase = os.path.split(self.bambase)[1]
@@ -11,7 +12,11 @@ class SplitBAM(object):
         self.batchsize = batchsize
         self.directory = directory
         self.index = index
-        self.region = region
+        if regions == None:
+            regions = [(None,None,None)]
+        self.regions = regions
+        if limit == None or limit <= 0:
+            limit = 1e+20
         self.limit = limit
 
     def normalize_readgroup(self,rg):
@@ -25,38 +30,39 @@ class SplitBAM(object):
         allrg = {}
         rgindex = 0
         limit = self.limit
-        if limit == None or limit <= 0:
-            limit = 1e+20
         seenrg = set()
         while True:
-            outsam = dict()
+            outal = defaultdict(set)
             more=False
             samfile = pysam.AlignmentFile(self.bamfile, "rb", require_index=False)
-            if self.region:
-                readiter = samfile.fetch(contig=self.region[0],start=self.region[1],stop=self.region[2],until_eof=True)
-            else:
-                readiter = samfile.fetch(until_eof=True)
-            for al in readiter:
+            for reg in sorted(self.regions):
+              try:
+                aliter =  samfile.fetch(contig=reg[0],start=reg[1],stop=reg[2],until_eof=True)
+              except ValueError:
+                # Bad contig names...
+                continue
+              for al in aliter:
                 rg = self.readgroups.group(al)
                 if rg and rg not in allrg:
                     allrg[rg] = rgindex
                     rgindex += 1
                 if rg and rg not in seenrg:
-                    if rg not in outsam:
-                        if len(outsam) >= min(self.batchsize,limit):
+                    if rg not in outal:
+                        if len(outal) >= min(self.batchsize,limit):
                             more = True
                             continue
-                        else:
-                            rgfilename = self.readgroup_filename(rg)
-                            outsam[rg] = (rgfilename,pysam.AlignmentFile(rgfilename, "wb", template=samfile))
-                    outsam[rg][1].write(al)
-            for rg in outsam:
-                outsam[rg][1].close()
+                    outal[rg].add(al)
+            for rg in outal:
+                rgfilename = self.readgroup_filename(rg)
+                outsam = pysam.AlignmentFile(rgfilename, "wb", template=samfile)
+                for al in sorted(outal[rg],key=lambda al: (al.reference_id,al.reference_start,al.reference_end)):
+                    outsam.write(al)
+                outsam.close()
                 if self.index:
-                    pysam.index(outsam[rg][0])
-                yield rg,outsam[rg][0],allrg[rg],len(allrg)
-            seenrg.update(outsam)
-            limit = limit - len(outsam)
-            outsam = dict()
+                    pysam.index(rgfilename)
+                yield rg,rgfilename,allrg[rg],min(self.limit,len(allrg))
+            seenrg.update(outal)
+            limit = limit - len(outal)
+            outal = defaultdict(set)
             if not more or limit <= 0:
                 break
