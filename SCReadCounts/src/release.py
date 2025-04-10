e16527a31b46578d167c2957538e31ff663384d9
Nathan Edwards <edwardsnj@gmail.com>
2025-04-10 12:37:53 -0400

    update version numbers

--- SCReadCounts/src/release.py
+++ SCReadCounts/src/release.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 RELEASE = "SCReadCounts"
-VERSION = '1.4.0'
+VERSION = '1.4.1'
 PROGRAMS = 'readCountsMatrix.py scReadCounts.py readCounts.py varLoci.py scVarLoci.py rcio.py scCountLoci.py exonicFilter.py'
 INCLUDES = 'common ReadCounts'
 if __name__ == '__main__':
