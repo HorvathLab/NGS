1df4cc37b09a99c0506f230186dec936f5523cb4
Nathan Edwards <edwardsnj@gmail.com>
2025-04-11 15:45:53 -0400

    update version for conda

--- SCReadCounts/src/release.py
+++ SCReadCounts/src/release.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 RELEASE = "SCReadCounts"
-VERSION = '1.4.1'
+VERSION = '1.4.2'
 PROGRAMS = 'readCountsMatrix.py scReadCounts.py readCounts.py varLoci.py scVarLoci.py rcio.py scCountLoci.py exonicFilter.py'
 INCLUDES = 'common ReadCounts'
 if __name__ == '__main__':
